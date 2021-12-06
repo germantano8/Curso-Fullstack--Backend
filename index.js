d0fa6ba3429110321cda4836992aeb2899b48190
Germán Tano <germantano88@gmail.com>
2021-12-06 10:38:19 -0300

    Agregada Procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
 const router = require('./src/routes/index.js');
 
